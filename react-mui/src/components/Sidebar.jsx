9daa234ea2629e7cb0b113907a77c2f0e2a582a6
Robin Haider <robinhaider69@gmail.com>
2023-06-22 14:56:25 +0600

    react mui - sidebar

--- react-mui/src/components/Sidebar.jsx
+++ react-mui/src/components/Sidebar.jsx
@@ -1,9 +1,90 @@
-import { Box } from '@mui/material';
+import { Home } from '@mui/icons-material';
+import ArticleRoundedIcon from '@mui/icons-material/ArticleRounded';
+import GroupIcon from '@mui/icons-material/Group';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
 
 const Sidebar = () => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary='Inbox' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <ArticleRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary='Pages' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary='Groups' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <StorefrontIcon />
+            </ListItemIcon>
+            <ListItemText primary='Marketplace' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <Diversity3Icon />
+            </ListItemIcon>
+            <ListItemText primary='Friends' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary='Profile' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#'>
+            <ListItemIcon>
+              <DarkModeIcon />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
